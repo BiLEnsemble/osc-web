0d0a59f3b5ef4949621f2803e3eab80fd6a97407
Holger Ballweg <holger@music-nerds.net>
2014-02-12 13:02:21 +0000

    removed unnecessary stuff

--- bridge.js
+++ bridge.js
@@ -1,21 +1,22 @@
 var osc = require('node-osc'),
     io = require('socket.io').listen(8081);
 
-var oscServer, oscClient;
+var oscClient;
 
 io.sockets.on('connection', function (socket) {
-  socket.on("config", function (obj) {
-    oscServer = new osc.Server(obj.server.port, obj.server.host);
-    oscClient = new osc.Client(obj.client.host, obj.client.port);
+	socket.on("config", function (obj) {
+		oscClient = new osc.Client(obj.client.host, obj.client.port);
 
-    oscClient.send('/status', socket.sessionId + ' connected');
-
-    oscServer.on('message', function(msg, rinfo) {
-      console.log(msg, rinfo);
-//		socket.emit("message", { addr: msg, msg: msg });
-    });
-  });
-  socket.on("message", function (obj) {
-      oscClient.send(obj.addr, obj.msg);
-  });
+		oscClient.send('/status', socket.sessionId + ' connected');
+		console.log("Connected: ", oscClient);
+	});
+	socket.on("message", function (addr, msg) {
+		if (typeof oscClient !== 'undefined') {
+			oscClient.send(addr, msg);
+		}
+		else
+		{
+			console.log("oscClient undefined! not sending: ", addr, msg);
+		}
+	});
 });
